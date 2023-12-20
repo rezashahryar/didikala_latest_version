34b077943f1ee9abb62ceb246b9affe47d96d3b7
reza shahryar <reza.shahryarineya7851@gmail.com>
2023-12-20 18:26:46 +0330

    implement pagination for blog section

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import get_object_or_404
 from django.views import generic
-from .models import Blog
+from .models import Blog, Category
+
+
 # Create your views here.
 
 
 class BlogListView(generic.ListView):
+    queryset = Blog.published.all()
     template_name = 'blog/blog_list.html'
     context_object_name = 'blogs'
+    paginate_by = 2
 
-    def get_queryset(self):
-        return Blog.published.all()
 
 
 class BlogDetailView(generic.DetailView):
     context_object_name = 'blog'
+
     def get_object(self):
         slug = self.kwargs.get('slug')
         return get_object_or_404(Blog, slug=slug, status=True)
+
+
+class CategoryObjectsView(generic.ListView):
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'blogs'
+    paginate_by = 12
+
+    def get_queryset(self):
+        cat_name = self.kwargs.get('cat_name')
+        category = get_object_or_404(Category, name=cat_name)
+        return category.blogs.filter(status=True)
