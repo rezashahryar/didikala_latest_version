9218e1113bad818a8370d1806e8442b07db5a79e
reza shahryar <reza.shahryarineya7851@gmail.com>
2023-12-25 19:09:39 +0330

    update products/models.py

--- products/models.py
+++ products/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-
-# Create your models here.
-
-
-from django.db import models
 from django.urls import reverse
 from random import uniform
 from django.core.validators import MinValueValidator, MaxValueValidator
