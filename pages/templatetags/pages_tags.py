3ddaa7ea2979d8e3ddd5f64a5111dfc3b8995aea
reza shahryar <reza.shahryarineya7851@gmail.com>
2024-01-21 00:25:39 +0330

    update query for product count

--- pages/templatetags/pages_tags.py
+++ pages/templatetags/pages_tags.py
@@ -1,20 +1,11 @@
 from django import template
 from products.models import ProductCategory, Product
+from django.db.models import Count
 
 register = template.Library()
 
 
 @register.inclusion_tag("includes/product_category_objects.html")
 def product_categories():
-    # global categories
-    products = Product.list.select_related('category')
-    categories = ProductCategory.objects.all()
-    cat_dict = {}
-    for name in categories:
-        cat_dict[name] = products.filter(category=name).count()
-    return {'categories': cat_dict}
-
-
-# @register.inclusion_tag('includes/table_of_categories.html')
-# def categories():
-#     return {'categories_all': categories}
+    categories = ProductCategory.objects.prefetch_related('products').annotate(products_count=Count('products'))
+    return {'categories': categories}
